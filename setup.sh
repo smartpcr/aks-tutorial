731043396a0be4695dbf9fb3225eb51cecee521d
xiaodong li <xiaodongli@xiaodongs-MBP.lan>
2018-06-28 09:23:33 -0700

    mac setup help/draft

--- setup.sh
+++ setup.sh
@@ -1,18 +1,49 @@
 # brew update
 brew update 
 
+# manually install the following
+# 1. jdk
+# 2. docker
+# 3. python3
+# 4. nodejs
+
 echo "installing azure cli"
 brew install azure-cli
-az version 
+az -v  
 
 echo "installing kubernetes-cli"
 brew install kubectl
 
+echo "installing virtualbox"
+brew cask install virtualbox
+
 echo "installing minikube"
 brew cask install minikube 
+
+echo "verifying..."
+docker --version 
+docker-compose --version 
+docker-machine --version 
 minikube version 
+kubectl version --client 
+
+echo "starting minikue..."
 minikube start 
-kubectl get pods
+kubectl cluster-info 
+kubectl get nodes 
 
 echo "installing helm"
 brew install kubernetes-helm
+
+echo "installing draft"
+brew tap azure/draft && brew install draft
+draft init 
+eval $(minikube docker-env)
+
+kubectl create -f ./helm-rbac.yaml 
+helm init --service-account tiller
+helm search
+helm repo update
+
+echo "installing wordpress..."
+helm install stable/wordpress
